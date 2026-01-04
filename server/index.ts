ed2acef546aec3ece475d1df05a603b80a9c9346
IBNAY <ibnayfield@IBNAYs-Air.lan>
2026-01-04 20:01:27 +0530

    fix: allow all cors origins to support hostinger preview domains

--- server/index.ts
+++ server/index.ts
@@ -13,7 +13,8 @@ const allowedOrigins = [
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
-  if (origin && allowedOrigins.includes(origin)) {
+  // Allow any origin to connect (for now, to fix Hostinger deployment issues)
+  if (origin) {
     res.setHeader("Access-Control-Allow-Origin", origin);
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept");
